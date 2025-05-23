e4266efa22e9dd24cef08a3a17f6b991d3bce911
barbariannumber2 <yuuki31.courage@gmail.com>
2025-05-24 03:23:31 +0900

    イベントシステム関連機能、プレハブ追加

---
+++ Assets/PracticeGame/Common/EventSystem/EventSystemDeduplicator.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PracticeGame
+{
+    /// <summary>
+    /// イベントシステムの重複を排除するクラス
+    /// ExtenjectのProjectContextでEventSystemと一緒にシングルトン生成する想定
+    /// </summary>
+    [RequireComponent(typeof(EventSystem))]
+    public class EventSystemDeduplicator : MonoBehaviour
+    {
+        /// <summary>
+        /// GetComponentで取得しなくていいようにInspectorから設定する
+        /// </summary>
+        [SerializeField]
+        private EventSystem thisEventSystem;
+
+
+
+        private void Awake()
+        {
+            //ExtenjectのAsSingleで生成した場合、DontDestroyOnLoadに属するオブジェクトの子として生成されるため
+            //AwakeのタイミングでシーンがDontDestroyOnLoadになる　これによりヒエラルキー配置と区別可能
+            if (gameObject.scene.name != "DontDestroyOnLoad")
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            EventSystem.current = thisEventSystem;
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDestroy()
+        {
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+        {
+            Dedupe();
+        }
+
+        private void Dedupe()
+        {
+            EventSystem.current = thisEventSystem;
+
+            EventSystem[] eventSystems = FindObjectsByType<EventSystem>(FindObjectsSortMode.None);
+
+            if (eventSystems.Length <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < eventSystems.Length; i++)
+            {
+                var eventSystemObject = eventSystems[i].gameObject;
+                if (eventSystemObject.scene == null || eventSystemObject.scene.name == "DontDestroyOnLoad")
+                {
+                    continue;
+                }
+
+                if (eventSystems[i] == EventSystem.current)
+                {
+                    continue;
+                }
+#if UNITY_EDITOR
+                Debug.LogWarning($"Extra EventSystem exist in scene: {eventSystemObject.scene.name}");
+                Debug.Log($"Destroying extra EventSystem: {eventSystemObject.name}");
+#endif
+                Destroy(eventSystemObject);
+                eventSystems[i] = null;
+
+            }
+        }
+    }
+}
---
+++ Assets/PracticeGame/Common/EventSystem/EventSystemInstaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace PracticeGame
+{
+    [CreateAssetMenu(fileName = "EventSystemInstaller", menuName = "Installers/MyInstallers/EventSystemInstaller")]
+    public class EventSystemInstaller : ScriptableObjectInstaller<EventSystemInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<EventSystem>()
+            .FromComponentInNewPrefabResource("EventSystem")
+            .AsSingle()
+            .NonLazy();
+        }
+    }
+}
