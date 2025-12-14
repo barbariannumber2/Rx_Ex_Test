4459334f225ecd9cbc63ad89658a6a50f05866c8
barbariannumber2 <yuuki31.courage@gmail.com>
2025-12-15 00:32:14 +0900

    playSceneへのデータ受け渡しクラス作成

---
+++ Assets/PracticeGame/Play/PlaySceneData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PracticeGame
+{
+    public class PlaySceneData : ISceneData
+    {
+        public Difficulty DifficultyLevel { get; private set; }
+        
+        public PlaySceneData(Difficulty difficulty)
+        {
+            DifficultyLevel = difficulty;
+        }
+    }
+}
