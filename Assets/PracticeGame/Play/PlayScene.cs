6bdc41d571da122d821aca8bd68837c3a4d0ae36
barbariannumber2 <yuuki31.courage@gmail.com>
2025-12-15 00:34:04 +0900

    とりあえず受け取った難易度データをログに表示

--- Assets/PracticeGame/Play/PlayScene.cs
+++ Assets/PracticeGame/Play/PlayScene.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
 
 namespace PracticeGame
 {
@@ -7,6 +9,15 @@ namespace PracticeGame
         public override SceneType GetSceneType()
         {
            return SceneType.Play;
+        }
+
+        public override UniTask OnInitialize(ISceneData sceneData, CancellationToken token)
+        {
+            PlaySceneData playSceneData = sceneData as PlaySceneData;
+
+            Debug.Log(playSceneData.DifficultyLevel.ToString());
+
+            return base.OnInitialize(sceneData, token);
         }
     }
 }
